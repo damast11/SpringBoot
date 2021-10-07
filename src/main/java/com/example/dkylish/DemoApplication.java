0d73ee3028252b19b135e92e2eb7efa7586dfba1
Dmytro Kulish <Dmytro_Kulish@epam.com>
2021-10-07 20:59:56 +0300

    init commit

--- src/main/java/com/example/dkylish/DemoApplication.java
+++ src/main/java/com/example/dkylish/DemoApplication.java
@@ -2,12 +2,8 @@ package com.example.dkylish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@ComponentScan("com.example.dkylish")
 public class DemoApplication {
 
 	public static void main(String[] args) {
